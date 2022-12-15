0e7286a398e4332592b606eeda9c7cc74cbcd7e2
Kristopher Woodall <kriswoodall@me.com>
2022-12-15 14:33:30 -0600

    Initial Commit

---
+++ Logo Generator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Logo Generator
+//
+//  Created by Kristopher Woodall on 12/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
