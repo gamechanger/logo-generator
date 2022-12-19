c7e99ced6b0aa9f8e09b4cf4f83675d98cccb95a
Joshua Sharo <josh.sharo@gc.com>
2022-12-19 17:02:27 -0500

    Fixed directory structure

--- Logo Generator/LogoGeneratorViewController.swift
+++ Logo Generator/LogoGeneratorViewController.swift
@@ -18,27 +18,28 @@ enum LogoGeneratorError: Error {
 class LogoGeneratorViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let imageView = UIImageView(image: generate())
-//        view.addSubview(imageView)
-//        imageView.snp.makeConstraints { make in
-//            make.centerX.centerY.equalToSuperview()
-//        }
-        let generatorView = UIHostingController(rootView: PlayerAvatarGeneratorView(model: .init()))
-        view.addSubview(generatorView.view)
-        generatorView.view.snp.makeConstraints { make in
-            make.left.top.right.bottom.equalToSuperview()
+        let imageView = UIImageView(image: generate())
+        view.addSubview(imageView)
+        imageView.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
         }
+//        let generatorView = UIHostingController(rootView: PlayerAvatarGeneratorView(model: .init()))
+//        view.addSubview(generatorView.view)
+//        generatorView.view.snp.makeConstraints { make in
+//            make.left.top.right.bottom.equalToSuperview()
+//        }
     }
 
     func generate() -> UIImage? {
-        guard let path = Bundle.main.resourceURL else {
+        guard let path = Bundle.main.path(forResource: "Models", ofType: nil, inDirectory: nil) else {
             fatalError("Fatal error: failed to find the CoreML models.")
         }
+        let resourceUrl = URL(fileURLWithPath: path)
         do {
             guard #available(iOS 16.2, *) else {
                 throw LogoGeneratorError.unsupportedOS
             }
-            let pipeline = try StableDiffusionPipeline(resourcesAt: path)
+            let pipeline = try StableDiffusionPipeline(resourcesAt: resourceUrl)
             try pipeline.loadResources()
             guard let image = try pipeline.generateImages(prompt: "beach with palm trees", seed: -1).compactMap({ $0 }).first else {
                 throw LogoGeneratorError.generateImagesFailed
